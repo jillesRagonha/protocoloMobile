7549ea869ed8360d164660b3752b22a807af2040
jillesRagonha <jilles.ragonha@hotmail.com>
2018-11-22 15:38:27 -0200

    criando dashboard

---
+++ app/src/main/java/br/com/agilles/protocolo/ui/activity/Dashboard.kt
@@ -0,0 +1,26 @@
+package br.com.agilles.protocolo.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import br.com.agilles.protocolo.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class Dashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        configNavDrawer()
+    }
+
+    fun configNavDrawer() {
+        var header = navigation_view.getHeaderView(0)
+        val toogle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.abrir, R.string.fechar)
+        toogle.drawerArrowDrawable.color = ContextCompat.getColor(this, R.color.colorTextIcons)
+        drawer_layout.addDrawerListener(toogle)
+        toogle.syncState()
+    }
+}
