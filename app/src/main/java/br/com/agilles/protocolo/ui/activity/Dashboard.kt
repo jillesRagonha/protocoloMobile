7d930bd72dcae8174e5d516d0789a30eb78d5253
jillesRagonha <jilles.ragonha@hotmail.com>
2018-11-24 02:15:49 -0200

    criando dashboard

--- app/src/main/java/br/com/agilles/protocolo/ui/activity/Dashboard.kt
+++ app/src/main/java/br/com/agilles/protocolo/ui/activity/Dashboard.kt
@@ -1,9 +1,10 @@
 package br.com.agilles.protocolo.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import br.com.agilles.protocolo.R
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -14,7 +15,14 @@ class Dashboard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         configNavDrawer()
+        configuraFab()
     }
+
+
+    fun configuraFab(){
+    }
+
+
 
     fun configNavDrawer() {
         var header = navigation_view.getHeaderView(0)
